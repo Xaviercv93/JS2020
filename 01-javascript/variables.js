14ab888aaf35c4a1da9abb45022012ca2c30be5a
xaviercv93 <xaviercv93@gmail.com>
2020-10-31 04:33:46 -0500

    falsy truty

--- 01-javascript/variables.js
+++ 01-javascript/variables.js
@@ -34,15 +34,54 @@ console.log(typeof numero, typeof sueldo, typeof texto, typeof booleanoo,
     typeof hijo, typeof zapatos);
 
 //La clase Number me devuelve un number y comprueba si algo pasado como argumento es un numero
-console.log(typeof Number("javi"),Number("javi"));
+console.log(typeof Number("javi"), Number("javi"));
 
 
 
+//truty y falsy
+if (true == true) {
+    console.log("es verdad");
+} else {
+    console.log("es verdad");
+}
 
+//falsy = cadena de caracteres vacia
+if ("") {
+    console.log("es truty")
+} else {
+    console.log("es falsy")
+}
 
+//truty = cadena de caracteres con algo
+if ("jajaja") {
+    console.log("es truty")
+} else {
+    console.log("es falsy")
+}
 
+//truty = numeros negativos
 
+if (-1) {
+    console.log("es truty")
+} else {
+    console.log("es falsy")
+}
 
+//falsy = el numero 0
+if (0) {
+    console.log("es truty")
+} else {
+    console.log("es falsy")
+}
 
+//truty = numeros positivos
+
+if (1) {
+    console.log("es truty")
+} else {
+    console.log("es falsy")
+}
+
+console.log("siempre comprobar la existencia de los falsy \"\" 0");
 
 
